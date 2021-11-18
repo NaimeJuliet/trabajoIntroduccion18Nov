30866532058056117f676062d5710b354d6e6d0e
NaimeJuliet <naime2008@hotmail.com>
2021-11-18 08:29:04 -0500

    mi trabajo 18 de noviembre

---
+++ form.js
@@ -0,0 +1,34 @@
+const form = document.querySelector("form")
+
+
+
+console.log(form)
+
+
+if(form){
+    console.log('it is a form');
+
+    form.addEventListener('submit', function(event){
+        console.log('Sending');
+
+      
+        event.preventDefault()
+
+        const email = document.querySelector('#email')
+        const password = form.querySelector('#password')
+        const check = form['check']
+
+        if(email && password && check){
+            if(email.value == '' || email.value == null){
+                alert('ERROR: Empty email')
+            }else if(password.value == '' || password.value == null){
+                alert('ERROR: Empty password')
+            }else if(!check.checked){
+                alert('ERROR: Acepta terminos y condiciones')
+            }else{
+                console.log('Verified');
+                form.submit()
+            }
+        }
+    })
+}
